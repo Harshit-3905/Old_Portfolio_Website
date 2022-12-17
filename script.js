088a101e2f88ea5e5ff541b296bf6edfe6ebf50b
Harshit-3905 <harshitjoshileet@gmail.com>
2022-12-17 23:49:10 +0530

    Version 1.0

--- script.js
+++
@@ -1,7 +0,0 @@
-var loader = document.getElementsByID("preloader");
-window.addEventListener("load", function(){ 
-      window.onscroll = function(){window.scrollTo(0,0)};
-})
-window.addEventListener("load", function(){
-    loader.style.display = "none";
-})
